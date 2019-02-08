fe2c84568dba2e3808bd1e97e745c7dc1dbfee25
Cameron Smith <smithc11@rpi.edu>
2019-02-08 08:19:13 -0500

    hack to pass a struct to kernel

--- hello.cc
+++ hello.cc
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "ints.h"
+
 void hello(int& rg_nverts, int& rg_degree,
     int& rg_n, int* rg_i,
     int& g_nverts, int& g_max_deg,
-    int& g_off_n, int* g_off_i,
+    void* g_off,
     int& g_adj_n, int* g_adj_i)
 {
   #pragma HLS INTERFACE m_axi port=rg_nverts bundle=gmem0 offset=slave
@@ -13,8 +15,7 @@ void hello(int& rg_nverts, int& rg_degree,
   #pragma HLS INTERFACE m_axi port=rg_i bundle=gmem3 offset=slave
   #pragma HLS INTERFACE m_axi port=g_nverts bundle=gmem4 offset=slave
   #pragma HLS INTERFACE m_axi port=g_max_deg bundle=gmem5 offset=slave
-  #pragma HLS INTERFACE m_axi port=g_off_n bundle=gmem6 offset=slave
-  #pragma HLS INTERFACE m_axi port=g_off_i bundle=gmem7 offset=slave
+  #pragma HLS INTERFACE m_axi port=g_off bundle=gmem6 offset=slave
   #pragma HLS INTERFACE m_axi port=g_adj_n bundle=gmem8 offset=slave
   #pragma HLS INTERFACE m_axi port=g_adj_i bundle=gmem9 offset=slave
   #pragma HLS INTERFACE s_axilite port=rg_nverts
@@ -23,17 +24,16 @@ void hello(int& rg_nverts, int& rg_degree,
   #pragma HLS INTERFACE s_axilite port=rg_i
   #pragma HLS INTERFACE s_axilite port=g_nverts
   #pragma HLS INTERFACE s_axilite port=g_max_deg
-  #pragma HLS INTERFACE s_axilite port=g_off_n
-  #pragma HLS INTERFACE s_axilite port=g_off_i
+  #pragma HLS INTERFACE s_axilite port=g_off
   #pragma HLS INTERFACE s_axilite port=g_adj_n
   #pragma HLS INTERFACE s_axilite port=g_adj_i
-  struct ints {
-    int n;
-    int* i;
-  } iarr;
+
+#include "ints.h"
+  ints iarr;
   iarr.n = 5;
   iarr.i = new int[5];
-  printf("%s rg_nverts %d iarr.n %d\n", __func__, rg_nverts, iarr.n);
+  ints* g = (ints*) g_off;
+  printf("%s rg_nverts %d iarr.n %d g->n %d\n", __func__, rg_nverts, iarr.n, g->n);
   delete [] iarr.i;
 }
 
@@ -44,16 +44,17 @@ int main() {
   int* rg_i = new int[8];
   int g_nverts = 2;
   int g_max_deg = 6;
-  int g_off_n = 12;
-  int* g_off_i = new int[12];
   int g_adj_n = 12;
   int* g_adj_i = new int[12];
+  ints g_off;
+  g_off.n = 12;
+  g_off.i = new int[12];
   hello(rg_nverts, rg_degree, rg_n, rg_i,
     g_nverts, g_max_deg,
-    g_off_n, g_off_i,
+    &g_off,
     g_adj_n, g_adj_i);
   delete [] rg_i;
-  delete [] g_off_i;
+  delete [] g_off.i;
   delete [] g_adj_i;
   return 0;
 }
---
+++ ints.h
@@ -0,0 +1,7 @@
+#ifndef INTS_H_
+#define INTS_H_
+struct ints {
+  int n;
+  int* i;
+};
+#endif
