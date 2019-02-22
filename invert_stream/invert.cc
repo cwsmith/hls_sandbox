5cf45e39c11f4929d817fde3142632add15a0042
myuan <myuan2009@gmail.com>
2019-02-22 01:40:41 -0500

    pipeline try 2

--- invert_stream/invert.cc
+++ invert_stream/invert.cc
@@ -5,88 +5,98 @@
 using hlslib::Stream;
 
 #define MAX_ADJ 32
-/*
-void invert(int numPar, int chldPerPar, int numChld,
-            int* parToChld, int* chldToPar, int* chldDeg)
-{
-  #pragma HLS INTERFACE m_axi     port=parToChld  bundle=gmem    offset=slave
-  #pragma HLS INTERFACE m_axi     port=chldToPar  bundle=gmem1   offset=slave
-  #pragma HLS INTERFACE m_axi     port=chldDeg    bundle=gmem2   offset=slave
-  #pragma HLS INTERFACE s_axilite port=numPar     bundle=control
-  #pragma HLS INTERFACE s_axilite port=chldPerPar bundle=control
-  #pragma HLS INTERFACE s_axilite port=numChld    bundle=control
-
-  //no idea how to size these
-  Stream<int> deg;
-  Stream<int> edge;
-
-  edge: for(int i=0; i<numPar*chldPerPar; i++) {
-     #pragma HLS PIPELINE
-     const int parent = i / chldPerPar;
-     const int child = parToChld[i];
-     edge.Push(parent);
-     edge.Push(child);
-  }
-
-  insert: for(int i=0; i<numPar*chldPerPar; i++) {
-     #pragma HLS PIPELINE
-     const int parent = edge.Pop();
-     const int child = edge.Pop();
-     const int idx = chldDeg[child];
-     deg.Push(idx);
-     deg.Push(child);
-     chldToPar[child*MAX_ADJ+idx] = parent;
-  }
-
-  incdegree: for(int i=0; i<numPar*chldPerPar; i++) {
-     #pragma HLS PIPELINE
-     const int idx = deg.Pop();
-     const int child = deg.Pop();
-     chldDeg[child] = idx+1;
-     //printf("i %d c %d idx %d deg %d\n", i, child, idx, chldDeg[child]);
-  }
-}
-*/
-void get_edges(Stream<int> &st_in, Stream<int> &st_out, int numPar, int chldPerPar){
-  //int center = st_in.Pop();
-  for (int i=0; i<numPar*chldPerPar; i++){
-    #pragma HLS PIPELINE II=1
-    //Read(POp)
-    int child=st_in.Pop();
-    //Compute
-    int parent = i/chldPerPar;
-    //Write(Push)
-    st_out.Push(parent);
-    st_out.Push(child);
-
-  }
-
-}
-
-void invert_edges(Stream<int> & st_in, Stream<int> &st_out, int numPar, int chldPerPar, int* chldDeg, int* chldToPar){
-  #pragma HLS INTERFACE m_axi     port=chldToPar  bundle=gmem1   offset=slave
-  #pragma HLS INTERFACE m_axi     port=chldDeg    bundle=gmem2   offset=slave
-  for (int i=0; i<numPar*chldPerPar; i++ ){
-    #pragma HLS PIPELINE II=1
-    //Read twice for a par-chld pair
-    int parent=st_in.Pop();
-    int child=st_in.Pop();
-    //Compute 1: Populate chldToPar array
-    chldToPar[child*MAX_ADJ+chldDeg[child]]=parent;
-    //Compute 2: Update chldDeg
-    chldDeg[child]++;
-    //Write (1 for finished, totally irrelevant to compute steps)
-    st_out.Push(1);
-  }
-
-}
 
 void ReadMem(int const *in, Stream<int> &s){
   for (int i=0; i<6; ++i){
-    #pragma HLS PIPELINE II=1
-    s.Push(in[i]);
+   #pragma HLS PIPELINE II=1
+   s.Push(in[i]);
+ }
+}
+
+void get_edges(Stream<int> &st_in, Stream<int> &st_out, int numPar, int chldPerPar){
+ //int center = st_in.Pop();
+ for (int i=0; i<numPar*chldPerPar; i++){
+   #pragma HLS PIPELINE II=1
+   //Read(POp)
+   int child=st_in.Pop();
+   //Compute
+   int parent = i/chldPerPar;
+   //Write(Push)
+   st_out.Push(parent);
+   st_out.Push(child);
+
   }
 }
+
+void invert_edges(Stream<int> & st_in, Stream<int> &st_out, int numPar, int chldPerPar, int* chldDeg, int* chldToPar){
+ #pragma HLS INTERFACE m_axi     port=chldToPar  bundle=gmem1   offset=slave
+ #pragma HLS INTERFACE m_axi     port=chldDeg    bundle=gmem2   offset=slave
+ for (int i=0; i<numPar*chldPerPar; i++ ){
+   #pragma HLS PIPELINE II=1
+   //Read twice for a par-chld pair
+   int parent=st_in.Pop();
+   int child=st_in.Pop();
+   //Compute 1: Populate chldToPar array
+   chldToPar[child*MAX_ADJ+chldDeg[child]]=parent;
+   //Compute 2: Update chldDeg
+   chldDeg[child]++;
+   //Write (1 for finished, totally irrelevant to compute steps)
+   st_out.Push(1);
+ }
+}
+void invert(Stream<int> &pipe0, Stream<int> &pipe1, Stream<int> &pipe2,
+             int numPar, int chldPerPar, int numChld,
+             int* parToChld, int* chldToPar, int* chldDeg)
+{
+      #pragma HLS INTERFACE m_axi     port=parToChld  bundle=gmem    offset=slave
+      #pragma HLS INTERFACE m_axi     port=chldToPar  bundle=gmem1   offset=slave
+      #pragma HLS INTERFACE m_axi     port=chldDeg    bundle=gmem2   offset=slave
+      #pragma HLS INTERFACE s_axilite port=numPar     bundle=control
+      #pragma HLS INTERFACE s_axilite port=chldPerPar bundle=control
+      #pragma HLS INTERFACE s_axilite port=numChld    bundle=control
+    /*
+     //no idea how to size these
+      Stream<int> deg;
+     Stream<int> edge;
+  
+  edge: for(int i=0; i<numPar*chldPerPar; i++) {
+        #pragma HLS PIPELINE
+        const int parent = i / chldPerPar;
+        const int child = parToChld[i];
+        edge.Push(parent);
+        edge.Push(child);
+     }
+   
+     insert: for(int i=0; i<numPar*chldPerPar; i++) {
+        #pragma HLS PIPELINE
+        const int parent = edge.Pop();
+        const int child = edge.Pop();
+        const int idx = chldDeg[child];
+        deg.Push(idx);
+        deg.Push(child);
+        chldToPar[child*MAX_ADJ+idx] = parent;
+    }
+  
+  incdegree: for(int i=0; i<numPar*chldPerPar; i++) {
+        #pragma HLS PIPELINE
+        const int idx = deg.Pop();
+        const int child = deg.Pop();
+        chldDeg[child] = idx+1;
+     //printf("i %d c %d idx %d deg %d\n", i, child, idx, chldDeg[child]);
+     }*/
+  HLSLIB_DATAFLOW_INIT();
+  //ParToChld As the 1st input stream
+  HLSLIB_DATAFLOW_FUNCTION(ReadMem, parToChld, pipe0);
+  
+  //Start concurrent pipelines (x2?)
+  //1st output stream: pairs of par and chld (size 12) push into pipe[1]
+  get_edges(pipe0,pipe1,numPar,chldPerPar);
+  //1st output -> 2nd input: pipe1, update Deg and ChldtoPar(final results),output nonsense into pipe2
+  invert_edges(pipe1,pipe2,numPar,chldPerPar,chldDeg,chldToPar);
+}
+
+
+
 
 int main() {
   int numPar = 3;
@@ -97,9 +107,11 @@ int main() {
   int* chldDeg = new int[numChld];
   for(int i=0; i<numChld; i++)
     chldDeg[i] = 0;
- // invert(numPar, chldPerPar, numChld, parToChld, chldToPar, chldDeg);
+
 
   Stream<int> pipe[3];
+ 
+  invert(pipe[0],pipe[1],pipe[2],numPar, chldPerPar, numChld, parToChld, chldToPar, chldDeg);
   
   HLSLIB_DATAFLOW_INIT();
 
