5b4253ead4291d3f318684f740e4894065f7bd7d
myuan <myuan2009@gmail.com>
2019-02-22 10:04:55 -0500

    compiled and ?working?

--- invert_stream/invert_workinginStreambutNotpipelined.cc
+++ invert_stream/invert_workinginStreambutNotpipelined.cc
@@ -14,35 +14,9 @@ void ReadMem(int const *in, Stream<int> &s){
 }
 
 void get_edges(Stream<int> &st_in, Stream<int> &st_out, int numPar, int chldPerPar){
- //int center = st_in.Pop();
- for (int i=0; i<numPar*chldPerPar; i++){
-   #pragma HLS PIPELINE II=1
-   //Read(POp)
-   int child=st_in.Pop();
-   //Compute
-   int parent = i/chldPerPar;
-   //Write(Push)
-   st_out.Push(parent);
-   st_out.Push(child);
-
-  }
 }
 
 void invert_edges(Stream<int> & st_in, Stream<int> &st_out, int numPar, int chldPerPar, int* chldDeg, int* chldToPar){
- #pragma HLS INTERFACE m_axi     port=chldToPar  bundle=gmem1   offset=slave
- #pragma HLS INTERFACE m_axi     port=chldDeg    bundle=gmem2   offset=slave
- for (int i=0; i<numPar*chldPerPar; i++ ){
-   #pragma HLS PIPELINE II=1
-   //Read twice for a par-chld pair
-   int parent=st_in.Pop();
-   int child=st_in.Pop();
-   //Compute 1: Populate chldToPar array
-   chldToPar[child*MAX_ADJ+chldDeg[child]]=parent;
-   //Compute 2: Update chldDeg
-   chldDeg[child]++;
-   //Write (1 for finished, totally irrelevant to compute steps)
-   st_out.Push(1);
- }
 }
 void invert(
              int numPar, int chldPerPar, int numChld,
@@ -54,17 +28,46 @@ void invert(
       #pragma HLS INTERFACE s_axilite port=numPar     bundle=control
       #pragma HLS INTERFACE s_axilite port=chldPerPar bundle=control
       #pragma HLS INTERFACE s_axilite port=numChld    bundle=control
-  
-  Stream<int> pipe[3];
+  Stream<int> children(6);
+  Stream<int> edge(12); 
+  Stream<int> pipe3(6);
   HLSLIB_DATAFLOW_INIT();
   //ParToChld As the 1st input stream
-  HLSLIB_DATAFLOW_FUNCTION(ReadMem, parToChld,pipe[0]);
+  //HLSLIB_DATAFLOW_FUNCTION(ReadMem, parToChld,pipe0);
+  for (int i=0; i<6; ++i){
+   #pragma HLS PIPELINE II=1
+   children.Push(parToChld[i]);
+ }
   
   //Start concurrent pipelines (x2?)
   //1st output stream: pairs of par and chld (size 12) push into pipe[1]
-  HLSLIB_DATAFLOW_FUNCTION(get_edges, pipe[0],pipe[1],numPar,chldPerPar);
+ // HLSLIB_DATAFLOW_FUNCTION(get_edges, pipe0,pipe1,numPar,chldPerPar);
+ //int center = st_in.Pop();
+ for (int i=0; i<numPar*chldPerPar; i++){
+   #pragma HLS PIPELINE II=1
+   //Read(POp)
+   int child=children.Pop();
+   //Compute
+   int parent = i/chldPerPar;
+   //Write(Push)
+   edge.Push(parent);
+   edge.Push(child);
+
+  }
   //1st output->2nd input: pipe1, update Deg and ChldtoPar(final results),output nonsense into pipe2
-  HLSLIB_DATAFLOW_FUNCTION(invert_edges, pipe[1],pipe[2],numPar,chldPerPar,chldDeg,chldToPar);
+  //HLSLIB_DATAFLOW_FUNCTION(invert_edges, pipe1,pipe2,numPar,chldPerPar,chldDeg,chldToPar);
+ for (int i=0; i<numPar*chldPerPar; i++ ){
+   #pragma HLS PIPELINE II=1
+   //Read twice for a par-chld pair
+   int parent=edge.Pop();
+   int child=edge.Pop();
+   //Compute 1: Populate chldToPar array
+   chldToPar[child*MAX_ADJ+chldDeg[child]]=parent;
+   //Compute 2: Update chldDeg
+   chldDeg[child]++;
+   //Write (1 for finished, totally irrelevant to compute steps)
+   pipe3.Push(1);
+ }
 }
 
 
@@ -81,7 +84,9 @@ int main() {
     chldDeg[i] = 0;
 
 
+  Stream<int> pipe[3];
  
+  //invert(pipe[0],pipe[1],pipe[2],numPar, chldPerPar, numChld, parToChld, chldToPar, chldDeg);
   invert(numPar, chldPerPar, numChld, parToChld, chldToPar, chldDeg);
  
   //print results
