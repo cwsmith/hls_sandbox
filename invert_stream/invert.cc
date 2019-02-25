90a4dccb9d628fc2b83285b88c27bea218649437
Cameron Smith <smithc11@rpi.edu>
2019-02-25 12:47:29 -0500

    shift children, don't compute degree

--- invert_stream/invert.cc
+++ invert_stream/invert.cc
@@ -28,42 +28,53 @@ void get_edges(Stream<int> &st_in, Stream<int> &st_out,
   }
 }
 
-void invert_edges(Stream<int> & st_in,
-    int numPar, int chldPerPar, int* chldDeg, int* chldToPar){
+void invert_edges(Stream<int> & st_in, Stream<int> &st_out,
+    int numPar, int chldPerPar, int* chldToPar){
   invert_edges: for (int i=0; i<numPar*chldPerPar; i++ ){
     #pragma HLS PIPELINE II=1
     //Read twice for a par-chld pair
     const int parent=st_in.Pop();
     const int child=st_in.Pop();
-    const int d = chldDeg[child];
     //Compute 1: Populate chldToPar array
-    chldToPar[child*MAX_ADJ+d]=parent;
-    //Compute 2: Update chldDeg
-    chldDeg[child] = d+1;
+    chldToPar[child*MAX_ADJ]=parent;
+    st_out.Push(child);
+  }
+}
+
+void shiftChildren(Stream<int> & st_in,
+  int numPar, int chldPerPar, int* chldToPar) {
+  for (int i=0; i<numPar*chldPerPar; i++ ){
+    const int child=st_in.Pop();
+    shiftChildren: for(int j=MAX_ADJ-1; j>0; --j) {
+      #pragma HLS UNROLL
+      chldToPar[child*MAX_ADJ+j] = chldToPar[child*MAX_ADJ+(j-1)];
+    }
   }
 }
 
 void invert(int numPar, int chldPerPar, int numChld,
-    int* parToChld, int* chldToPar, int* chldDeg) {
+    int* parToChld, int* chldToPar) {
   #pragma HLS INTERFACE m_axi     port=parToChld  bundle=gmem    offset=slave
   #pragma HLS INTERFACE m_axi     port=chldToPar  bundle=gmem1   offset=slave
-  #pragma HLS INTERFACE m_axi     port=chldDeg    bundle=gmem2   offset=slave
   #pragma HLS INTERFACE s_axilite port=numPar     bundle=control
   #pragma HLS INTERFACE s_axilite port=chldPerPar bundle=control
   #pragma HLS INTERFACE s_axilite port=numChld    bundle=control
 
   Stream<int> childStrm("children");
   Stream<int> edgeStrm("edges");
+  Stream<int> childShiftStrm("childShift");
   HLSLIB_DATAFLOW_INIT();
   //ParToChld As the 1st input stream
   HLSLIB_DATAFLOW_FUNCTION(ReadMem,parToChld,childStrm);
   HLSLIB_DATAFLOW_FUNCTION(get_edges,childStrm,edgeStrm,numPar,chldPerPar);
-  HLSLIB_DATAFLOW_FUNCTION(invert_edges,edgeStrm,numPar,chldPerPar,
-      chldDeg,chldToPar);
+  HLSLIB_DATAFLOW_FUNCTION(invert_edges,edgeStrm,childShiftStrm,numPar,
+      chldPerPar,chldToPar);
+  HLSLIB_DATAFLOW_FUNCTION(shiftChildren,childShiftStrm,numPar,chldPerPar,
+      chldToPar);
   HLSLIB_DATAFLOW_FINALIZE();
 }
 
-void checkSoln(int* chldToPar, int* chldDeg, int numChld, int numPar) {
+void checkSoln(int* chldToPar, int numChld, int numPar) {
   int* cToP = new int[numChld*numPar];
   cToP[0*numChld+0] = 1;
   cToP[0*numChld+1] = 1;
@@ -74,9 +85,9 @@ void checkSoln(int* chldToPar, int* chldDeg, int numChld, int numPar) {
 
   //print results
   for(int child=0; child<numChld; child++) {
-    for(int j=0; j<chldDeg[child]; j++) {
+    for(int j=0; j<MAX_ADJ; j++) {
       const int parent = chldToPar[child*MAX_ADJ+j];
-      if( ! cToP[child*numChld+parent] ) {
+      if( parent != -1 && ! cToP[child*numChld+parent] ) {
         fprintf(stderr, "graph edge child %d - parent %d"
             "does not exist in reference solution\n",
             child, parent);
@@ -96,9 +107,11 @@ int main() {
   int* chldDeg = new int[numChld];
   for(int i=0; i<numChld; i++)
     chldDeg[i] = 0;
+  for(int i=0; i<numChld*MAX_ADJ; i++)
+    chldToPar[i] = -1;
 
-  invert(numPar, chldPerPar, numChld, parToChld, chldToPar, chldDeg);
-  checkSoln(chldToPar, chldDeg, numChld, numPar);
+  invert(numPar, chldPerPar, numChld, parToChld, chldToPar);
+  checkSoln(chldToPar, numChld, numPar);
 
   delete [] chldToPar;
   delete [] chldDeg;
