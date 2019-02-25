acc7c359d54342bd4086e9de69fb509e6c607ace
Cameron Smith <smithc11@rpi.edu>
2019-02-25 15:56:10 -0500

    use datapack for adj array

--- invert_stream/invert.cc
+++ invert_stream/invert.cc
@@ -1,78 +1,108 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <cassert>
 #include "hlslib/Stream.h"
+#include "hlslib/DataPack.h"
 #include "hlslib/Simulation.h"
 using hlslib::Stream;
 
 #define MAX_ADJ 32
 
-void ReadMem(int const *in, Stream<int> &s, const int N){
-  read: for (int i=0; i<N; ++i){
+using Adj_t = hlslib::DataPack<int, MAX_ADJ>;
+
+void readChildren(int const *in, Stream<int> &c_out, Stream<int> &c2_out, const int N){
+  readChildren: for (int i=0; i<N; ++i){
     #pragma HLS PIPELINE
-    s.Push(in[i]);
+    const int chld = in[i];
+    c_out.Push(chld);
+    c2_out.Push(chld);
   }
 }
 
-void invert_edges(Stream<int> & st_in, Stream<int> &st_out,
-    const int N, const int chldPerPar, int* chldToPar){
+void readAdj(Adj_t const *chldToPar, Stream<int> &s_in, Stream<Adj_t>& s_out, const int N){
+  readAdj: for (int i=0; i<N; ++i){
+    #pragma HLS PIPELINE
+    const int child = s_in.Pop();
+    s_out.Push(chldToPar[child]);
+  }
+}
+
+void invert_edges(Stream<Adj_t> & adj_in, Stream<Adj_t> &adj_out,
+    const int N, const int chldPerPar){
   invert_edges: for (int i=0; i<N; i++ ){
     #pragma HLS PIPELINE
-    const int child=st_in.Pop();
+    Adj_t adj = adj_in.Pop();
     const int parent = i/chldPerPar;
-    chldToPar[child*MAX_ADJ]=parent;
-    st_out.Push(child);
+    adj[0] = parent;
+    adj_out.Push(adj);
   }
 }
 
-void shiftChildren(Stream<int> & st_in,
-  const int N, int* chldToPar) {
+void shiftChildren(Stream<int> & chld_in, Stream<Adj_t> & adj_in,
+  const int N, Adj_t* chldToPar) {
   for (int i=0; i<N; i++ ){
-    const int child=st_in.Pop();
+    const int child = chld_in.Pop();
+    const Adj_t adj = adj_in.Pop();
     shiftChildren: for(int j=MAX_ADJ-1; j>0; --j) {
       #pragma HLS UNROLL
-      chldToPar[child*MAX_ADJ+j] = chldToPar[child*MAX_ADJ+(j-1)];
+      chldToPar[child][j] = adj[j-1];
     }
   }
 }
 
 void invert(int numPar, int chldPerPar, int numChld,
-    int* parToChld, int* chldToPar) {
+    int* parToChld, Adj_t* chldToPar) {
   #pragma HLS INTERFACE m_axi     port=parToChld  bundle=gmem    offset=slave
   #pragma HLS INTERFACE m_axi     port=chldToPar  bundle=gmem1   offset=slave
   #pragma HLS INTERFACE s_axilite port=numPar     bundle=control
   #pragma HLS INTERFACE s_axilite port=chldPerPar bundle=control
   #pragma HLS INTERFACE s_axilite port=numChld    bundle=control
 
-  Stream<int> childStrm("children");
-  Stream<int> childShiftStrm("childShift");
+  Stream<int> chld_s("children");
+  Stream<int> chld2_s("children2");
+  Stream<Adj_t> chldAdj_s("childAdj");
+  Stream<Adj_t> chldAdj2_s("childAdj");
+  Stream<int> adjShift_s("adjShift");
   const int N = numPar*chldPerPar;
   HLSLIB_DATAFLOW_INIT();
   //ParToChld As the 1st input stream
-  HLSLIB_DATAFLOW_FUNCTION(ReadMem,parToChld,childStrm,N);
-  HLSLIB_DATAFLOW_FUNCTION(invert_edges,childStrm,childShiftStrm,N,
-      chldPerPar,chldToPar);
-  HLSLIB_DATAFLOW_FUNCTION(shiftChildren,childShiftStrm,N,chldToPar);
+  HLSLIB_DATAFLOW_FUNCTION(readChildren,parToChld,chld_s,chld2_s,N);
+  HLSLIB_DATAFLOW_FUNCTION(readAdj,chldToPar,chld_s,chldAdj_s,N);
+  HLSLIB_DATAFLOW_FUNCTION(invert_edges,chldAdj_s,chldAdj2_s,N,
+      chldPerPar);
+  HLSLIB_DATAFLOW_FUNCTION(shiftChildren,chld2_s,chldAdj2_s,N,chldToPar);
   HLSLIB_DATAFLOW_FINALIZE();
 }
 
-void checkSoln(int* chldToPar, int numChld, int numPar) {
+void checkSoln(Adj_t* chldToPar, int numChld, int numPar) {
   int* cToP = new int[numChld*numPar];
+  /*0:A,B,C*/
   cToP[0*numChld+0] = 1;
   cToP[0*numChld+1] = 1;
   cToP[0*numChld+2] = 1;
+  /*1:A*/
   cToP[1*numChld+0] = 1;
+  /*2:B,C*/
   cToP[2*numChld+1] = 1;
   cToP[2*numChld+2] = 1;
 
   //print results
   for(int child=0; child<numChld; child++) {
     for(int j=0; j<MAX_ADJ; j++) {
-      const int parent = chldToPar[child*MAX_ADJ+j];
-      if( parent != -1 && ! cToP[child*numChld+parent] ) {
-        fprintf(stderr, "graph edge child %d - parent %d"
-            "does not exist in reference solution\n",
-            child, parent);
-        exit(EXIT_FAILURE);
+      printf("i %d j %d ", child, j);
+      const int parent = chldToPar[child][j];
+      if( parent != -1 ) {
+        assert(parent>=0 && parent<numPar);
+        const int refPar = cToP[child*numChld+parent];
+        printf("child %d parent %d refPar %d\n", child, parent, refPar);
+        if( ! refPar ) {
+          fprintf(stderr, "graph edge child %d - parent %d"
+              "does not exist in reference solution\n",
+              child, parent);
+          exit(EXIT_FAILURE);
+        }
+      } else {
+        printf("\n");
       }
     }
   }
@@ -84,17 +114,14 @@ int main() {
   int chldPerPar = 2;
   int numChld = 3;
   int parToChld[6] = {/*A*/1,0,/*B*/0,2,/*C*/2,0};
-  int* chldToPar = new int[numChld*MAX_ADJ];
-  int* chldDeg = new int[numChld];
+  Adj_t* chldToPar = new Adj_t[numChld];
   for(int i=0; i<numChld; i++)
-    chldDeg[i] = 0;
-  for(int i=0; i<numChld*MAX_ADJ; i++)
-    chldToPar[i] = -1;
+    for(int j=0; j<MAX_ADJ; j++)
+      chldToPar[i][j] = -1;
 
   invert(numPar, chldPerPar, numChld, parToChld, chldToPar);
   checkSoln(chldToPar, numChld, numPar);
 
   delete [] chldToPar;
-  delete [] chldDeg;
   return 0;
 }
