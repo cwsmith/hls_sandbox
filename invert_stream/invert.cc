629930a3a9fb41a26d0083eea2361b65c045caae
Cameron Smith <smithc11@rpi.edu>
2019-03-13 17:30:30 -0400

    stream from shift to readAdj to avoid race
    the write by shiftChildren and subsequent read by readAdj from main memory was a race condition

--- invert_stream/invert.cc
+++ invert_stream/invert.cc
@@ -19,34 +19,49 @@ void readChildren(int const *in, Stream<int> &c_out, Stream<int> &c2_out, const 
   }
 }
 
-void readAdj(Adj_t const *chldToPar, Stream<int> &s_in, Stream<Adj_t>& s_out, const int N){
+void readAdj(Adj_t const *chldToPar, Stream<int> &c_in, Stream<int>& cshift_in,
+    Stream<Adj_t>& s_out, Stream<int> &c_out, const int N){
   readAdj: for (int i=0; i<N; ++i){
     #pragma HLS PIPELINE
-    const int child = s_in.Pop();
-    s_out.Push(chldToPar[child]);
+    const int child = c_in.Pop();
+    if(i>0) {
+      //Avoid reading stale adj arrays from childToPar
+      // by waiting until shiftChildren has finished
+      // its write.
+      const int shiftedChild = cshift_in.Pop();
+    }
+    Adj_t adj = chldToPar[child];
+    //shift
+    for(int j=MAX_ADJ-1; j>0; --j) {
+      adj.Set(j, adj.Get(j-1));
+    }
+    adj.Set(0,-1);
+
+    s_out.Push(adj);
+    c_out.Push(child);
   }
 }
 
-void invert_edges(Stream<Adj_t> & adj_in, Stream<Adj_t> &adj_out,
+void invert_edges(Stream<int> &c_in, Stream<Adj_t> & adj_in, Stream<Adj_t> &adj_out,
     const int N, const int chldPerPar){
   invert_edges: for (int i=0; i<N; i++ ){
     #pragma HLS PIPELINE
     Adj_t adj = adj_in.Pop();
+    const int child = c_in.Pop();
     const int parent = i/chldPerPar;
-    adj[0] = parent;
+    adj.Set(0,parent);
     adj_out.Push(adj);
   }
 }
 
 void shiftChildren(Stream<int> & chld_in, Stream<Adj_t> & adj_in,
-  const int N, Adj_t* chldToPar) {
+  const int N, Adj_t* chldToPar, Stream<int>& chld_out) {
   shiftChildren: for (int i=0; i<N; i++ ){
     #pragma HLS PIPELINE
     const int child = chld_in.Pop();
     const Adj_t adj = adj_in.Pop();
-    for(int j=MAX_ADJ-1; j>0; --j) {
-      chldToPar[child][j] = adj[j-1];
-    }
+    chldToPar[child] = adj;
+    chld_out.Push(child);
   }
 }
 
@@ -60,20 +75,54 @@ void invert(int numPar, int chldPerPar, int numChld,
 
   Stream<int> chld_s("children");
   Stream<int> chld2_s("children2");
+  Stream<int> chld3_s("children3");
+  Stream<int> chld4_s("children4");
   Stream<Adj_t> chldAdj_s("childAdj");
   Stream<Adj_t> adjShift_s("adjShift");
   const int N = numPar*chldPerPar;
   HLSLIB_DATAFLOW_INIT();
   HLSLIB_DATAFLOW_FUNCTION(readChildren,parToChld,chld_s,chld2_s,N);
-  HLSLIB_DATAFLOW_FUNCTION(readAdj,chldToPar,chld_s,chldAdj_s,N);
-  HLSLIB_DATAFLOW_FUNCTION(invert_edges,chldAdj_s,adjShift_s,N,
+  HLSLIB_DATAFLOW_FUNCTION(readAdj,chldToPar,chld_s,chld4_s,chldAdj_s,chld3_s,N);
+  HLSLIB_DATAFLOW_FUNCTION(invert_edges,chld3_s,chldAdj_s,adjShift_s,N,
       chldPerPar);
-  HLSLIB_DATAFLOW_FUNCTION(shiftChildren,chld2_s,adjShift_s,N,chldToPar);
+  HLSLIB_DATAFLOW_FUNCTION(shiftChildren,chld2_s,adjShift_s,N,chldToPar,chld4_s);
   HLSLIB_DATAFLOW_FINALIZE();
+  chld4_s.Pop(); //clear the final child
 }
 
-void checkSoln(Adj_t* chldToPar, int numChld, int numPar) {
+void checkSoln(Adj_t* chldToPar, int* cToP, int numChld, int numPar) {
+  //print results
+  for(int child=0; child<numChld; child++) {
+    for(int j=0; j<MAX_ADJ; j++) {
+      const int parent = chldToPar[child].Get(j);
+      if( parent != -1 ) {
+        assert(parent>=0 && parent<numPar);
+        printf("child %d parent %d\n", child, parent);
+        const int refPar = cToP[child*numChld+parent];
+        if( ! refPar ) {
+          fprintf(stderr, "graph edge child %d - parent %d"
+              "does not exist in reference solution\n",
+              child, parent);
+          exit(EXIT_FAILURE);
+        }
+      }
+    }
+  }
+}
+
+void test1() {
+  int numPar = 3;
+  int chldPerPar = 2;
+  int numChld = 3;
+  int parToChld[6] = {/*A*/1,0,/*B*/0,2,/*C*/2,0};
+
+  Adj_t* chldToPar = new Adj_t[numChld];
+  for(int i=0; i<numChld; i++)
+    chldToPar[i].Fill(-1);
+
   int* cToP = new int[numChld*numPar];
+  for(int i=0; i<numChld*numPar; i++)
+    cToP[i] = 0;
   /*0:A,B,C*/
   cToP[0*numChld+0] = 1;
   cToP[0*numChld+1] = 1;
@@ -84,39 +133,47 @@ void checkSoln(Adj_t* chldToPar, int numChld, int numPar) {
   cToP[2*numChld+1] = 1;
   cToP[2*numChld+2] = 1;
 
-  //print results
-  for(int child=0; child<numChld; child++) {
-    for(int j=0; j<MAX_ADJ; j++) {
-      const int parent = chldToPar[child][j];
-      if( parent != -1 ) {
-        assert(parent>=0 && parent<numPar);
-        const int refPar = cToP[child*numChld+parent];
-        printf("child %d parent %d refPar %d\n", child, parent, refPar);
-        if( ! refPar ) {
-          fprintf(stderr, "graph edge child %d - parent %d"
-              "does not exist in reference solution\n",
-              child, parent);
-          exit(EXIT_FAILURE);
-        }
-      }
-    }
-  }
-  printf("solution matches\n");
+  invert(numPar, chldPerPar, numChld, parToChld, chldToPar);
+  checkSoln(chldToPar, cToP, numChld, numPar);
+  delete [] chldToPar;
+  delete [] cToP;
+  printf("done %s\n",__func__);
+}
+
+void test2() {
+  const int numPar = 5;
+  const int chldPerPar = 1;
+  const int numChld = 1;
+  int parToChld[numPar] = {/*A*/0,/*B*/0,/*C*/0,/*D*/0,/*E*/0};
+
+  Adj_t* chldToPar = new Adj_t[numChld];
+  for(int i=0; i<numChld; i++)
+    chldToPar[i].Fill(-1);
+
+  int* cToP = new int[numChld*numPar];
+  for(int i=0; i<numChld*numPar; i++)
+    cToP[i] = 0;
+
+  /*0:A*/
+  cToP[0*numChld+0] = 1;
+  /*0:B*/
+  cToP[1*numChld+0] = 1;
+  /*0:C*/
+  cToP[2*numChld+0] = 1;
+  /*0:D*/
+  cToP[3*numChld+0] = 1;
+  /*0:E*/
+  cToP[4*numChld+0] = 1;
+
+  invert(numPar, chldPerPar, numChld, parToChld, chldToPar);
+  checkSoln(chldToPar, cToP, numChld, numPar);
+  delete [] chldToPar;
+  delete [] cToP;
+  printf("done %s\n",__func__);
 }
 
 int main() {
-  int numPar = 3;
-  int chldPerPar = 2;
-  int numChld = 3;
-  int parToChld[6] = {/*A*/1,0,/*B*/0,2,/*C*/2,0};
-  Adj_t* chldToPar = new Adj_t[numChld];
-  for(int i=0; i<numChld; i++)
-    for(int j=0; j<MAX_ADJ; j++)
-      chldToPar[i][j] = -1;
-
-  invert(numPar, chldPerPar, numChld, parToChld, chldToPar);
-  checkSoln(chldToPar, numChld, numPar);
-
-  delete [] chldToPar;
+  test1();
+  test2();
   return 0;
 }
