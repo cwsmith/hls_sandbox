1aef0cb78d2b666cd573e2830fbe1e778d815ed4
myuan <myuan2009@gmail.com>
2019-04-15 14:28:18 -0400

    Added check solution function

--- intersect_stream/intersect.cc
+++ intersect_stream/intersect.cc
@@ -27,7 +27,6 @@ void readCompare(Adj_t adj2, Stream<int> &i_in, Stream<int> &i_out){
         i_out.Push(item);
         break;
       }
-    // if (flag==0) i_out.Push(-1);
     }
     if (flag==0) {i_out.Push(-1);}
   }
@@ -37,14 +36,26 @@ void writeResult(Adj_t &out, Stream<int> &i_in){
   WriteResult: for (int i=0;i<MAX_ADJ;i++){
     #pragma HLS PIPELINE
     const int item=i_in.Pop();
-    //AdjShift: for (int j=MAX_ADJ-1;j>0;--j){
-      //#pragma HLS PIPELINE
-      //out.Set(j,out.Get(j-1));
-    //}
     if (item!=-1) {out.Set(i,item);}
   }
 }
 
+void check_sol(Adj_t adj1, Adj_t adj2, Adj_t adj_out){
+  Adj_t adj_ref;
+  adj_ref.Fill(-1);
+  for (int i=0; i<MAX_ADJ; i++){
+    for (int j=0; j<MAX_ADJ;j++){
+      if (adj1.Get(i)==adj2.Get(j) and adj1.Get(i)!=-1)
+         adj_ref.Set(i,adj1.Get(i));
+    }
+  }
+  printf("\nReference Adj: ");
+  for (int i=0;i<MAX_ADJ;i++){
+    if (adj_ref.Get(i)!=-1){
+      printf("%d ",adj_ref.Get(i));
+    }
+  }
+}
 void intersect(Adj_t adj1, Adj_t adj2){
   #pragma HLS INTERFACE s_axilite	port=adj1	bundle=control
   #pragma HLS INTERFACE s_axilite	port=adj2	bundle=control
@@ -58,33 +69,34 @@ void intersect(Adj_t adj1, Adj_t adj2){
   HLSLIB_DATAFLOW_FUNCTION(readCompare,adj2,items1,items2);
   HLSLIB_DATAFLOW_FUNCTION(writeResult,adj_out,items2);
   HLSLIB_DATAFLOW_FINALIZE();
-  //items2.Pop(); 
  
-  printf("First Adj: %d ", adj1.Get(0));
-  for (int i=1;i<MAX_ADJ;i++){
+  printf("First Adj: ");
+  for (int i=0;i<MAX_ADJ;i++){
     if (adj1.Get(i)!=-1){
       printf("%d ",adj1.Get(i));
     }
   }
-  printf("\nSecond Adj: %d ", adj2.Get(0));
-  for (int i=1;i<MAX_ADJ;i++){
+  printf("\nSecond Adj: ");
+  for (int i=0;i<MAX_ADJ;i++){
     if (adj2.Get(i)!=-1){
       printf("%d ",adj2.Get(i));
     }
   }
-  printf("\nIntersect Adj: %d ", adj_out.Get(0));
-  for (int i=1;i<MAX_ADJ;i++){
+  printf("\nIntersect Adj: ");
+  for (int i=0;i<MAX_ADJ;i++){
     if (adj_out.Get(i)!=-1){
       printf("%d ",adj_out.Get(i));
     }
   }
+  check_sol(adj1,adj2,adj_out);
   printf("\n------Done------\n");
 }
+
 int main() {
   Adj_t adj1; //[Empty:-1,-1,-1...,-1]
   Adj_t adj2; //[0]
 
-  Adj_t adj3; //[0,1,2]
+  Adj_t adj3; //[0,1,5]
   Adj_t adj4; //[0,1,2,...,31]
 
   Adj_t adj5; //[0,1,2,3,4]
@@ -94,32 +106,24 @@ int main() {
   Adj_t adj8; //[3,1,0,2]
 
   adj1.Fill(-1);
-  adj2.Fill(-1);
-  adj2.Set(0,0);
-  adj3.Fill(-1);
-  adj3.Set(0,0);
-  adj3.Set(1,1);
-  adj3.Set(2,2);
-  for (int i=0;i<MAX_ADJ;i++){
-    adj4.Set(i,i);
-  }
+  
+  adj2.Fill(-1);adj2.Set(0,0);
+  
+  adj3.Fill(-1);adj3.Set(0,0);adj3.Set(1,1);adj3.Set(2,5);
+
+  for (int i=0;i<MAX_ADJ;i++) adj4.Set(i,i);
+  
   adj5.Fill(-1);
-  for (int i=0;i<5;i++){
-    adj5.Set(i,i);
-  }
+  for (int i=0;i<5;i++) adj5.Set(i,i);
+
   adj6.Fill(-1);
-  for (int i=0;i<3;i++){
-    adj6.Set(i,i);
-  }
+  for (int i=0;i<3;i++) adj6.Set(i,i);
+ 
   adj7.Fill(-1);
-  for (int i=0;i<4;i++){
-    adj7.Set(i,i);
-  }
-  adj8.Fill(-1);
-  adj8.Set(0,3);
-  adj8.Set(1,1);
-  adj8.Set(2,0);
-  adj8.Set(3,2);
+  for (int i=0;i<4;i++) adj7.Set(i,i);
+ 
+  adj8.Fill(-1); adj8.Set(0,3); adj8.Set(1,1); adj8.Set(2,0); adj8.Set(3,2);
+
   intersect(adj1,adj2);
   intersect(adj3,adj4);
   intersect(adj5,adj6);
